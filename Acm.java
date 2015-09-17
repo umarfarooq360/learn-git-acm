38b68167f05fe17de36a168b28fa1c3dc614fd40
umarfarooq360 <omar.farooq@richmond.edu>
2015-09-17 17:26:36 -0400

    First Commit

---
+++ Acm.java
@@ -0,0 +1,21 @@
+import java.util.*;
+/*
+	Simple Java class to show ACM members how to use Git
+	Author: Omar Farooq
+	Date: 9-17-15
+*/
+
+public class Acm{
+	private static String[] members = {
+		"Omar Farooq",
+		"Alexandru Pana",
+		"John Keto"
+	};
+
+	 public static void main(String[] args) {
+		System.out.println("ACM Members:\n"+ Arrays.toString(members).replaceAll(",", "\n"));	
+	}
+
+
+
+}
---
+++ build.sh
@@ -0,0 +1,2 @@
+javac Acm.java
+java Acm
